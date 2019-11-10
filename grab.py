8b236e91d92f263a49b96a8afd54e75c4fa50538
Chris Handy <maximinus@gmail.com>
2019-11-10 22:49:44 +0000

    Can grab from pre-downloaded html

--- grab.py
+++ grab.py
@@ -8,6 +8,32 @@ from songs import findSong
 URL = 'https://archive.org/details/gd1982-07-27.141510.sbd.pcm.streeter.dalton.miller.clugston.flac1644'
 
 
+class Show:
+	def __init__(self, item, title):
+		self.getVenue(title)
+		self.getDetails(item)
+
+	def getVenue(self, title):
+		try:
+			self.venue = title.split(':')[-1].strip()
+		except:
+			self.venue = "Unknown"
+
+	def getDetails(self, item):
+		self.title = item['data-id']
+		data = self.title.split('.')[0]
+		# result = gd1989-08-18
+		data = data[2:].split('-')
+		year = int(data[0])
+		month = int(data[1])
+		day = int(data[2])
+		self.date = date(year, month, day)
+
+	def __repr__(self):
+		date_format = self.date.strftime('%a %d %b %y')
+		recording = self.title.split('.')[-1]
+		return '{0}: {1}'.format(date_format, recording)
+
 class Track:
 	def __init__(self, name, duration, links):
 		# we can extract many things from this data
@@ -68,14 +94,24 @@ class Track:
 	def __repr__(self):
 		return('{: >30} {: >10} {: >12}'.format(self.song, self.getTimeString(), self.linkTxt()))
 
-def getBytesAndSave():
+
+def getYearData():
+	#url = 'https://archive.org/details/GratefulDead?&and[]=year%3A"1967"'
+	url = 'https://archive.org/search.php?query=collection%3AGratefulDead&page=1'
+	# perform a GET on this url
+	response = requests.get(url)
+	# save this reponse
+	with open('get.html', 'w') as foo:
+		foo.write(response.text)
+
+def getBytes():
 	response = requests.get(URL)
 	if response.status_code != 200:
 		print('  Error in response, got HTTP {0}'.format(response.status_code))
 		return
 	# response data is bytes, we need as a string
 	return response.text
-
+1
 def loadFile(name):
 	with open(name, 'r') as foo:
 		page = foo.read()
@@ -95,9 +131,37 @@ def extractPageData(page):
 		songs.append(Track(name, duration, links))
 	return songs
 
-if __name__ == '__main__':
-	page = loadFile('page.html')
-	songs = extractPageData(page)
-	for i in songs:
-		print(i)
+def loadYearData():
+	with open('get.html', 'r') as data_file:
+		year_data = data_file.read()
+	return year_data
 
+def extractYearData(year):
+	# get the results
+	soup = BeautifulSoup(year, features='html.parser')
+	results = soup.find('div', {'class': 'results'})
+	# all the shows we found, please
+	matches = results.findAll('div', class_='item-ia')
+	# filter this by ones which have a 'data-id'
+	shows = []
+	for i in matches:
+		data = i['data-id']
+		if data.startswith('gd19'):
+			shows.append(i)
+	# now grab the data we need
+	extracted_shows = []
+	for i in shows:
+		title = i.findAll('a')[1]['title']
+		extracted_shows.append(Show(i, title))
+	return extracted_shows
+
+if __name__ == '__main__':
+	#page = loadFile('page.html')
+	#songs = extractPageData(page)
+	#for i in songs:
+	#	print(i)
+	#getYearData()
+	data = loadYearData()
+	shows = extractYearData(data)
+	for i in shows:
+		print(i)
--- songs.py
+++ songs.py
@@ -2,27 +2,26 @@
 
 UNKNOWN_SONG = "UNKNOWN"
 
-SONGS = {UNKNOWN_SONG: [UNKNOWN_SONG],
-		 "Jack Straw":["Jack Straw"],
-		 "Sugaree": ["Sugaree"],
-		 "On The Road Again": ["On The Road Again"],
-		 "It Must Have Been The Roses": ["It Must Have Been The Roses"],
-		 "Me & My Uncle": ["Me & My Uncle"],
-		 "Big River": ["Big River"],
-		 "Peggy O": ["Peggy O"],
-		 "Little Red Rooster": ["Little Red Rooster"],
-		 "China Cat Sunflower": ["China Cat Sunflower"],
-		 "I Know You Rider": ["I Know You Rider"],
-		 "Tuning": ["Tuning"],
-		 "Playin' In The Band": ["Playin' In The Band"],
-		 "Terrapin Station": ["Terrapin Station"],
-		 "Playin' Jam": ["Playin' Jam"],
-		 "Drums": ["Drums"],
-		 "Space": ["Space"],
-		 "The Wheel": ["The Wheel"],
-		 "The Other One": ["The Other One"],
-		 "Stella Blue": ["Stella Blue"],
-		 "US Blues": ["US Blues"]}
+SONGS = [["Jack Straw"],
+		 ["Sugaree"],
+		 ["On The Road Again"],
+		 ["It Must Have Been The Roses"],
+		 ["Me & My Uncle"],
+		 ["Big River"],
+		 ["Peggy O"],
+		 ["Little Red Rooster"],
+		 ["China Cat Sunflower"],
+		 ["I Know You Rider"],
+		 ["Tuning"],
+		 ["Playin' In The Band"],
+		 ["Terrapin Station"],
+		 ["Playin' Jam"],
+		 ["Drums"],
+		 ["Space"],
+		 ["The Wheel"],
+		 ["The Other One"],
+		 ["Stella Blue"],
+		 ["US Blues"]]
 
 def showNewSong(name):
 	# massage data into correct format
@@ -30,9 +29,9 @@ def showNewSong(name):
 
 def findSong(name):
 	# iterate through the list
-	for song, names in SONGS.items():
-		if name in names:
-			return song
+	for song in SONGS:
+		if name in song:
+			return song[0]
 	# song not found, capture this
 	showNewSong(name)
 	return UNKNOWN_SONG
